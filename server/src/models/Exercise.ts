cea9dc8eb47a9189293113dee474afa7f815a40d
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-12-07 12:41:52 -0500

    full starter code maybe working lol

--- server/src/models/Exercise.ts
+++ server/src/models/Exercise.ts
@@ -1,34 +1,34 @@
-// import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
-// interface ExerciseDocument extends Document {
-//   name: string;
-//   muscle: string;
-//   difficulty: string;
-//   instructions: string;
-// }
+interface ExerciseDocument extends Document {
+  name: string;
+  muscle: string;
+  difficulty: string;
+  instructions: string;
+}
 
-// const ExerciseSchema = new Schema<ExerciseDocument>({
-//   name: {
-//     type: String,
-//     required: true
-//   },
+const ExerciseSchema = new Schema<ExerciseDocument>({
+  name: {
+    type: String,
+    required: true
+  },
   
-//   muscle: {
-//     type: String,
-//     required: true
-//   },
+  muscle: {
+    type: String,
+    required: true
+  },
  
-//   difficulty: {
-//     type: String,
-//     required: true
-//   },
-//   instructions: {
-//     type: String,
-//     required: true
-//   }
-// });
+  difficulty: {
+    type: String,
+    required: true
+  },
+  instructions: {
+    type: String,
+    required: true
+  }
+});
 
 
-// const Exercise = model<ExerciseDocument>('Exercise', ExerciseSchema);
+const Exercise = model<ExerciseDocument>('Exercise', ExerciseSchema);
 
-// export default Exercise;
+export default Exercise;
