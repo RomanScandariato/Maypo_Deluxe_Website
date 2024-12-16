9b896a892bcbb55f0f94ab7e32e1eca815c2683d
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-12-15 19:19:46 -0500

    removed dist deploy attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import {Routes, Route} from 'react-router-dom';
-import { useStore } from './store';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -14,15 +13,9 @@ import Landing from './pages/Landing';
 
 
 function App() {
-  const {state} = useStore()!;
 
   return (
     <>
-      {state.loading && (
-        <div className="loading-overlay d-flex justify-content-center align-items-center">
-          <h2 className="fw-light">Loading...</h2>
-        </div>
-      )}
 
       <Header />
 
