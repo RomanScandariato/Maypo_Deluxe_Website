376f6788263d498f989141be2d1a5181e46a9ce9
Mark Scandariato <music@maypodeluxe.com>
2025-09-02 18:15:05 -0400

    Added OrderCD page, form and updated Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Photos from './pages/Photos';
 import Socials from './pages/SocialMedia';
 import Contact from './pages/Contact';
 import Player from './pages/Player';
+import OrderCD from './pages/OrderCD';
+
 
 // import ProtectRoute from './components/ProtectRoute';
 
@@ -27,6 +29,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/songs" element={<Songs />} />
           <Route path="/photos" element={<Photos />} />
+          <Route path="/ordercd" element={<OrderCD />} />
           <Route path="/socials" element={<Socials />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/player" element={<Player />} />
